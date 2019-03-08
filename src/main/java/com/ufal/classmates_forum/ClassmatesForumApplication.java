becfd017e1fc8d8e5ccd797ff437da03af558509
Francisco <fvcneto.master98@gmail.com>
2019-03-08 14:17:35 -0300

    add springboot project :tada:

---
+++ src/main/java/com/ufal/classmates_forum/ClassmatesForumApplication.java
@@ -0,0 +1,13 @@
+package com.ufal.classmates_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmatesForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassmatesForumApplication.class, args);
+	}
+
+}
