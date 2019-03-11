e77ab2122c636b65ec4ece90d66b810be2243ca5
Francisco <fvcneto.master98@gmail.com>
2019-03-11 18:39:38 -0300

    create repository's directory and add a UserRepository

---
+++ api/src/main/java/com/ufal/classmates_forum/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ufal.classmates_forum.repository;
+
+import com.ufal.classmates_forum.domain.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+    
+}
