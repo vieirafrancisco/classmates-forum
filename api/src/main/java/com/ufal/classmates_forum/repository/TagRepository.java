34af50272f8f93213e1fefc749e92414b5634558
Francisco <fvcneto.master98@gmail.com>
2019-03-12 19:00:12 -0300

    Revert "create TagRepository"
    This reverts commit 6c0250a13f15b2ab648c109f68a9b1859d498232.

--- api/src/main/java/com/ufal/classmates_forum/repository/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ufal.classmates_forum.repository;
-
-import com.ufal.classmates_forum.domain.Tag;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Integer> {
-    
-}
