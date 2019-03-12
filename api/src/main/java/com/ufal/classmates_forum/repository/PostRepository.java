4accb3710392b282ac5004c91798fd3486278474
Francisco <fvcneto.master98@gmail.com>
2019-03-12 19:00:25 -0300

    Revert "create repository directory and add PostRepository"
    This reverts commit 510f10eb92641d12032a1350c6faa49c8cf95e81.

--- api/src/main/java/com/ufal/classmates_forum/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ufal.classmates_forum.repository;
-
-import com.ufal.classmates_forum.domain.Post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-    
-}
