6105a50959c3115504c9704a7a925c91939ff556
Francisco <fvcneto.master98@gmail.com>
2019-03-18 20:00:08 -0300

    request Login object in the /login and /logout routes

--- api/src/main/java/com/ufal/classmates_forum/controller/UserLoginController.java
+++ api/src/main/java/com/ufal/classmates_forum/controller/UserLoginController.java
@@ -1,6 +1,7 @@
 package com.ufal.classmates_forum.controller;
 
 import com.ufal.classmates_forum.UserLogin;
+import com.ufal.classmates_forum.domain.Login;
 import com.ufal.classmates_forum.domain.User;
 import com.ufal.classmates_forum.exceptions.UserAlreadyLoggedException;
 import com.ufal.classmates_forum.exceptions.UserNotLoggedException;
@@ -9,8 +10,8 @@ import com.ufal.classmates_forum.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,11 +20,14 @@ public class UserLoginController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping(value = "/login/{uid}")
-    public ResponseEntity<?> login(@PathVariable String uid){
+    @PostMapping(value = "/login")
+    public ResponseEntity<?> login(@RequestBody Login login){
+        String uid = login.getUID();
+        int id = login.getId();
+        
         try{
-            if(existsByUID(uid)){
-                UserLogin.getInstance().addUseUID(uid);
+            if(existsByIdAndUID(id, uid)){
+                UserLogin.getInstance().addUserUID(uid);
             } else{
                 return new ResponseEntity<>(
                     String.format("User not found!"),
@@ -38,29 +42,46 @@ public class UserLoginController {
         }
         
         return new ResponseEntity<>(
+            userRepository.findById(id),
             HttpStatus.OK
         );
     }
 
-    @PostMapping(value = "/logout/{uid}")
-    public ResponseEntity<?> logout(@PathVariable String uid){
-        try{
-            UserLogin.getInstance().removeUseUID(uid);
-            
-            return new ResponseEntity<>(
-                HttpStatus.OK
-            );
-        } catch(UserNotLoggedException e){
-            return new ResponseEntity<>(
-                e.getMessage(),
-                HttpStatus.NOT_FOUND
-            );
-        }
+    @PostMapping(value = "/logout")
+    public ResponseEntity<?> logout(@RequestBody Login login){
+        String uid = login.getUID();
+        int id = login.getId();
+        
+        if(existsByIdAndUID(id, uid)){
+            try{
+                UserLogin.getInstance().removeUserUID(uid);
+                
+                return new ResponseEntity<>(
+                    HttpStatus.OK
+                );
+            } catch(UserNotLoggedException e){
+                return new ResponseEntity<>(
+                    e.getMessage(),
+                    HttpStatus.NOT_FOUND
+                );
+            }
+        } 
+
+        return new ResponseEntity<>(
+            String.format("User don't exist"),
+            HttpStatus.NOT_FOUND
+        );
     } 
 
-    public boolean existsByUID(String uid){
-        for(User user: userRepository.findAll()){
-            if(user.getUID().equals(uid)) return true;
+    private boolean existsByIdAndUID(int id, String uid){
+        if(!userRepository.existsById(id)){
+            return false;
+        }
+
+        User user = userRepository.findById(id).get();
+
+        if(user.getUID().equals(uid)){
+            return true;
         }
         return false;
     }
