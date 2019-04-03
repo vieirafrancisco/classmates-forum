e2c7ae43865bb894a8e3a584686df415b94a6dec
jadsonlucio <jadsonaluno@hotmail.com>
2019-04-03 02:31:33 -0300

    add pair string class

---
+++ api/src/main/java/com/ufal/classmates_forum/Pair.java
@@ -0,0 +1,27 @@
+package com.ufal.classmates_forum;
+
+public class Pair {
+    private String first;
+    private String second;
+
+    public Pair(String first, String second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+}
